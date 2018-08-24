f1972b7630f50cb02520457491415995c6eda87b
Cloud Strife <mokryan17@gmail.com>
2018-08-24 00:51:15 -0500

    Completed process

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,8 +31,108 @@ function displayItems() {
                 "\nPrice: $" + res[i].price +
                 "\nStock Left: " + res[i].stock_quantity);
             console.log("============================");
-
         };
+        searchItems();
     })
 }
 displayItems();
+
+// ===== Purchase Process ===== //
+function searchItems() {
+    // Asks what you want to buy//
+    inquirer.prompt([
+        {
+            name: "choice",
+            message: "Which ID # would you like to buy?",
+            validate: function (val) {
+                if (isNaN(val) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function (answers) {
+        // Asks how many you want to buy //
+        inquirer.prompt([
+            {
+                name: "quantity",
+                message: "How many would you like to buy?",
+                validate: function (val) {
+                    if (isNaN(val) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ]).then(function (purchase) {
+            // Looks at chosen item //
+            connection.query("SELECT * FROM products WHERE id=" + "'" + answers.choice + "'", function (err, res) {
+                if (purchase.quantity <= res[0].stock_quantity) {
+                    var remainder = res[0].stock_quantity - purchase.quantity;
+                    var purchaseTotal = purchase.quantity * res[0].price;
+                    inquirer.prompt([
+                        {
+                            type: "list",
+                            name: "confirmPurchase",
+                            message: "Current sale is $" + purchaseTotal + ", is this right?",
+                            choices: ["Yes", "No"]
+                        }
+                    ]).then(function (complete) {
+                        // Updates inventory //
+                        if (complete.confirmPurchase === "Yes") {
+                            console.log("=============================================");
+                            console.log("Your purchase of " + res[0].product_name + " is on the way!");
+                            console.log("=============================================");
+                            connection.query("UPDATE products SET stock_quantity =" + remainder + " WHERE id=" + answers.choice + ";", function (err, res) {
+                                inquirer.prompt([
+                                    {
+                                        type: "list",
+                                        name: "buyMore",
+                                        message: "Do you want to buy something else?",
+                                        choices: ["Yes", "No"]
+                                    }
+                                    // Lets you buy more //
+                                ]).then(function(extraPurchase){
+                                    if(extraPurchase.buyMore === "Yes"){
+                                        displayItems();
+                                    }
+                                    // Ends cycle //
+                                    if(extraPurchase.buyMore === "No"){
+                                        console.log("=============================================");
+                                        console.log("Thank you for shopping with us! See you next time!");
+                                        console.log("=============================================");
+                                        process.exit(1);
+                                    }
+                                })
+                            })
+                        }
+                        // Start over //
+                        if (complete.confirmPurchase === "No"){
+                            console.log("=============================================");
+                            console.log("Thank you for shopping with us");
+                            console.log("=============================================");
+                            inquirer.prompt([
+                                {
+                                    type: "list",
+                                    name: "buyMore",
+                                    message: "Do you want to buy something else?",
+                                    choices: ["Yes", "No"]
+                                }
+                            ]).then(function(extraPurchase){
+                                if(extraPurchase.buyMore === "Yes"){
+                                    displayItems();
+                                }
+                                if(extraPurchase.buyMore === "No"){
+                                    console.log("=============================================");
+                                    console.log("Thank you for shopping with us! See you next time!");
+                                    console.log("=============================================");
+                                    process.exit(1);
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        })
+    })
+};
