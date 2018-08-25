de7bc860edbaa66dd000196510d5d9851e46a70e
Cloud Strife <mokryan17@gmail.com>
2018-08-25 15:17:22 -0500

    Added MySQL database

---
+++ SQL/bamazonSeeds.sql
@@ -0,0 +1,15 @@
+insert into products(product_name, department_name, price, stock_quantity)
+values("Hattori Hanzo Katana", "Sports", 10000, 46),
+("Cast Iron Skillet", "Cooking", 100, 46838),
+("V-Neck T-Shirt", "Clothing", 15, 648528),
+("Levi Blue Jeans", "Clothing", 60, 231564),
+("4K UHD TV", "Electronics", 800, 405658),
+("Nintendo Switch", "Entertainment", 300, 1064),
+("Dove Soap", "Body Care", 10, 10646541),
+("Asus Computer", "Electronics", 800, 60543),
+("YETI Coolers", "Camping", 200, 6451315),
+("Running Shoes", "Fitness", 120, 665415),
+("PS4 Pro", "Entertainment", 400, 5630),
+("ANKER Battery", "Electronics", 40, 234600)
+("Tradmill", "Fitness", 800, 100900),
+("Dyson Vaccum", "Cleaning", 300, 6000);
